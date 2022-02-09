d3765acb8c6ed1739cb7e8e26f83f374c397e20c
Imperialradim <radim.peterka.2018@gymbos.net>
2022-02-09 20:13:47 +0100

    ok

--- storage.js
+++ storage.js
@@ -1,10 +1,22 @@
 function saveSecret() {
     if (typeof(Storage) !== "undefined") {
-        sessionStorage.setItem("secret", "found")
+        localStorage.setItem("secret", "found")
+        const status = localStorage.getItem("secret")
+        if (status == "found") {
+            document.getElementById("secret2").innerHTML = "Die!"
+        }
     } else {
         alert("Not supported!")
     }
 }
 function removeSecret() {
-    sessionStorage.clear()
+    localStorage.clear()
+}
+
+function secreT() {
+    const status = localStorage.getItem("secret")
+    if (status == "found") {
+        document.getElementById("secret2").innerHTML = "Die!";
+        document.getElementById("secret").style.display = "none";
+    }
 }
